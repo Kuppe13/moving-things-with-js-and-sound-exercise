a2571e67874a49679932e7c6f9df28422cc03196
Kuppe13 <eaa25maku@students.eaaa.dk>
2025-11-05 20:03:32 +0100

    not finished

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+"use strict";
+
+const dodger = document.getElementById("dodger");
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();    
+    }
+
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+});
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+function moveDodgerUp() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom < 380) {
+    dodger.style.bottom = `${bottom + 1}px`;
+  }
+}
+
+function moveDodgerDown() {
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 1}px`;
+  }
+}
